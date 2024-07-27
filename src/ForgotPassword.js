abf0db909eb8a7855953448df04d01eb11ed3cbb
vijay <sit22it039@sairamtap.edu.in>
2024-07-27 19:45:05 +0530

    applied css

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,37 +1,50 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import './ForgotPassword.css';
+
 export default function ForgotPassword() {
-    const [result,setResult]=useState('');
-    const [username,setUsername]=useState('');
-    const [email,setEmail]=useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
 
-
-    const handleSubmit=async(e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.put(`http://127.0.0.1:5000/forgot_password`,{username,email});
+            const response = await axios.put('http://127.0.0.1:5000/forgot_password', { username, email });
             console.log(response.data);
             toast.success(response.data.message, { position: "top-right" });
-        }
-        catch(error){
+        } catch (error) {
             console.log("Error occurred during login:", error);
             toast.error("username or email invalid", { position: "top-right" });
         }
     }
-    return (
-        <div>
-            <h1>Forgot Password</h1>
-            <form onSubmit={handleSubmit}>
-                <label>username</label>
-                <input type="text" placeholder="user id" value={username} onChange={(e) => {setUsername(e.target.value.trim());}} />
 
-                <label>Email</label>
-                <input type="email" placeholder="Email" value={email} onChange={(e) => {setEmail(e.target.value.trim());}} />
-                <button type="submit" >Submit</button>
-            </form>
+    return (
+        <div className='container'>
+            <div className='formContainer'>
+                <div className='headingBox'>
+                    <h1>Forgot Password</h1>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <label>Username</label>
+                    <input 
+                        type="text" 
+                        placeholder="User ID" 
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value.trim())} 
+                    />
+                    <label>Email</label>
+                    <input 
+                        type="email" 
+                        placeholder="Email" 
+                        value={email} 
+                        onChange={(e) => setEmail(e.target.value.trim())} 
+                    />
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
             <ToastContainer />
         </div>
     );
-    }
+}
