48bc56d0f2714396ed911af770b294d315f6827b
vijay <sit22it039@sairamtap.edu.in>
2024-07-27 17:41:31 +0530

    completed forgot password feature

--- src/ForgotPassword.js
+++ src/ForgotPassword.js
@@ -1,15 +1,37 @@
-export default function forgotPassword() {
+import React,{useState} from 'react';
+import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+export default function ForgotPassword() {
+    const [result,setResult]=useState('');
+    const [username,setUsername]=useState('');
+    const [email,setEmail]=useState('');
+
+
+    const handleSubmit=async(e)=>{
+        e.preventDefault();
+        try {
+            const response = await axios.put(`http://127.0.0.1:5000/forgot_password`,{username,email});
+            console.log(response.data);
+            toast.success(response.data.message, { position: "top-right" });
+        }
+        catch(error){
+            console.log("Error occurred during login:", error);
+            toast.error("username or email invalid", { position: "top-right" });
+        }
+    }
     return (
         <div>
-        <h1>Forgot Password</h1>
-        <form>
-        <label>username</label>
-        <input type="text" placeholder="user id"></input>
-        <label>Email</label>
-        <input type="email" placeholder="Email"></input>
-        <button>Submit</button>
+            <h1>Forgot Password</h1>
+            <form onSubmit={handleSubmit}>
+                <label>username</label>
+                <input type="text" placeholder="user id" value={username} onChange={(e) => {setUsername(e.target.value.trim());}} />
 
-        </form>
+                <label>Email</label>
+                <input type="email" placeholder="Email" value={email} onChange={(e) => {setEmail(e.target.value.trim());}} />
+                <button type="submit" >Submit</button>
+            </form>
+            <ToastContainer />
         </div>
     );
     }
